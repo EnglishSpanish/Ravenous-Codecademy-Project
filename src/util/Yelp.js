b6cdc33e17cdd0cb232fa3214f6e7e1b365f294a
EnglishSpanish <64224000+EnglishSpanish@users.noreply.github.com>
2020-06-02 13:04:38 +0100

    Gtg

---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = 'k0E5H8q9CzWvnfjtRd0LcoNDoSLNX4AWPzWRZvIcGFiqlWZVOIghAUbeNQWHNCwJV3L2TjvpNo9La1bsQm7nfYuAph3kLSL7vF8voVWfYshuUv14jfSGsOjhIybWXnYx';
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json()
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                })
+            }
+        })
+    }
+}
+
+export default Yelp;
